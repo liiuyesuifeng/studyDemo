70a4832e849cfd4609a54c6a7c94b2881321c260
luhongru <973339083@qq.com>
2019-01-07 18:05:43 +0800

    修改

--- demo/src/main/java/com/example/demo/ThreadDomain/CriticalSection.java
+++ demo/src/main/java/com/example/demo/ThreadDomain/CriticalSection.java
@@ -10,48 +10,69 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 多线程临界区（在某个区域加锁）
+ * 代码块加锁比整个方法加锁效率高
  * @auto lhr
  */
 public class CriticalSection {
 
-    static  void testApproaches(PairManager pman1,PairManager pman2){
+    static  void testApproaches(PairManager pman1,PairManager pman2,PairManager pman3,PairManager pman4){
         ExecutorService exec = Executors.newCachedThreadPool();
-        PairManipulator pm1 = new PairManipulator(pman1);
-        PairManipulator pm2 = new PairManipulator(pman2);
-        PairChecker pCheck1 = new PairChecker(pman1);
-        PairChecker pCheck2 = new PairChecker(pman2);
-        exec.execute(pm1);
-        exec.execute(pm2);
-        exec.execute(pCheck1);
-        exec.execute(pCheck2);
-        try{
-            TimeUnit.MILLISECONDS.sleep(500);
-        }catch (InterruptedException ite){
-            ite.printStackTrace();
-            PrintUtil.print("Sleep interrupted");
-        }
-        PrintUtil.print("pm1" + pm1 + " \npm2" + pm2);
+       // for(int i=0;i<10;i++){
+            PairManipulator pm1 = new PairManipulator(pman1);
+            PairManipulator pm2 = new PairManipulator(pman2);
+            PairManipulator pm3 = new PairManipulator(pman3);
+            PairManipulator pm4 = new PairManipulator(pman4);
+            PairChecker pCheck1 = new PairChecker(pman1);
+            PairChecker pCheck2 = new PairChecker(pman2);
+            PairChecker pCheck3 = new PairChecker(pman3);
+            PairChecker pCheck4 = new PairChecker(pman4);
+            exec.execute(pm1);
+            exec.execute(pm2);
+            exec.execute(pm3);
+            exec.execute(pm4);
+            exec.execute(pCheck1);
+            exec.execute(pCheck2);
+            exec.execute(pCheck3);
+            exec.execute(pCheck4);
+            try{
+                TimeUnit.MILLISECONDS.sleep(500);
+            }catch (InterruptedException ite){
+                ite.printStackTrace();
+                PrintUtil.print("Sleep interrupted");
+            }
+            PrintUtil.print(" pm1" + pm1 + " \tpm2" + pm2 + "\t pm3"+pm3 );
+     //   }
+
+
         System.exit(0);
 
 
     }
     public static void main(String [] args){
-        PairManager pm1 = new PairManager1();
-        PairManager pm2 = new PairManager2();
-        testApproaches(pm1,pm2);
+        PairManager pm1 = new PairManager1("pm1");
+        PairManager pm2 = new PairManager2("pm2");
+        PairManager pm3 = new PairManager3("pm3");
+        PairManager pm4 = new PairManager3("pm4");
+        testApproaches(pm1,pm2,pm3,pm4);
+
     }
 }
 class Pair{
     private int x;
     private int y;
+    private String name;
     public Pair(int x,int y){
         this.x = x;
         this.y = y;
     }
-    public Pair(){}
+    public Pair(String name){
+        this.name = name;
+    }
     public int getX(){
         return x;
     }
@@ -65,13 +86,14 @@ class Pair{
         y++;
     }
 
+
     @Override
     public String toString() {
-        return "X ：" + x + "    Y : " + y;
+        return "name :"+ name + "X ：" + x + "    Y : " + y;
     }
     public class PairValuesNotEqualException extends RuntimeException{
         public PairValuesNotEqualException(){
-            super();
+            super("error" + Pair.this);
         }
     }
     public void checkState(){
@@ -82,8 +104,13 @@ class Pair{
 }
 abstract class PairManager{
     AtomicInteger checkCount = new AtomicInteger(0);
-    protected  Pair pair = new Pair();
+    protected String name;
+    protected  Pair pair = new Pair(name);
     private List<Pair> storage = Collections.synchronizedList(new ArrayList<>());
+    public PairManager(String name){
+        this.name = name;
+        PrintUtil.print(name);
+    }
     public synchronized Pair getPair(){
         return new Pair(pair.getX(),pair.getY());
     }
@@ -95,10 +122,15 @@ abstract class PairManager{
             ite.printStackTrace();
         }
     }
+    public String getName(){
+        return name;
+    }
     public abstract void increment();
 }
 class PairManager1 extends PairManager{
-
+    public PairManager1(String name){
+        super(name);
+    }
     @Override
     public synchronized void increment() {
         pair.incrementX();
@@ -107,6 +139,9 @@ class PairManager1 extends PairManager{
     }
 }
 class PairManager2 extends PairManager{
+    public PairManager2(String name){
+        super(name);
+    }
     @Override
     public void increment() {
         Pair temp;
@@ -114,6 +149,43 @@ class PairManager2 extends PairManager{
             pair.incrementX();
             pair.incrementY();
             temp = getPair();
+        }
+        store(temp);
+    }
+}
+class PairManager3 extends PairManager{
+    public PairManager3(String name){
+        super(name);
+    }
+    private  Lock lock = new ReentrantLock();
+    //不加synchronized关键字，X和Y 不相同校验抛出异常
+    @Override
+    public void increment() {
+        lock.lock();
+        try{
+            pair.incrementX();
+            pair.incrementY();
+            store(getPair());
+        }finally {
+            lock.unlock();
+        }
+    }
+}
+class PairManager4 extends PairManager{
+    public PairManager4(String name){
+        super(name);
+    }
+    private  Lock lock = new ReentrantLock();
+    @Override
+    public void increment() {
+        Pair temp;
+        lock.lock();
+        try{
+            pair.incrementX();
+            pair.incrementY();
+            temp = getPair();
+        }finally {
+            lock.unlock();
         }
         store(temp);
     }
@@ -128,12 +200,13 @@ class PairManipulator implements Runnable{
     public void run() {
         while(true){
             pm.increment();
+            PrintUtil.print(toString() + "\t\t" + Thread.currentThread().getName());
         }
     }
 
     @Override
     public String toString() {
-        return "pair :" + pm.getPair() + " checkCounter =" + pm.checkCount.get();
+        return "name : " + pm.getName() +"pair :" + pm.getPair() + " checkCounter =" + pm.checkCount.get();
     }
 }
 class PairChecker implements Runnable{
